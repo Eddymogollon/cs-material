3398498ad2aa85d2f81e1f6844a1a1560d109e8e
Eddy Mogollon <eddymogs@gmail.com>
2018-07-16 01:36:43 -0400

    first commit

---
+++ data_structures/queue/Main.java
@@ -0,0 +1,64 @@
+// Queue implementation of Java.
+
+import java.util.NoSuchElementException;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    MyQueue<Integer> q = new MyQueue<>();
+    System.out.println(q.isEmpty());
+    q.add(5);
+    q.add(3);
+    System.out.println(q.peek());
+    System.out.println(q.isEmpty());
+
+  }
+
+  public static class MyQueue<T> {
+
+    private static class QueueNode<T> {
+      private T data;
+      private QueueNode<T> next;
+
+      public QueueNode(T data) {
+        this.data = data;
+      }
+    }
+
+    private QueueNode<T> first;
+    private QueueNode<T> last;
+
+    public void add(T item) {
+      QueueNode<T> t = new QueueNode<T>(item);
+      if (last != null) {
+        last.next = t;
+      }
+      last = t;
+      if (first == null) {
+        first = last;
+      }
+    }
+
+    public T remove() {
+      if (first == null) throw new NoSuchElementException();
+      T data = first.data;
+      first = first.next;
+      if (first == null) {
+        last = null;
+      }
+      return data;
+    }
+
+    public T peek() {
+      if (first == null) throw new NoSuchElementException();
+      return first.data;
+    }
+
+    public boolean isEmpty() {
+      return first == null;
+    }
+  }
+
+}
